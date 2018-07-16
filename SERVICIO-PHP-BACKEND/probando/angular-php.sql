b76f243bb1619f6bc3b47d7d2d76280571771ab8
Joaquin <maza.joaquin@gmail.com>
2018-07-16 19:34:29 -0300

    limpio todo

--- SERVICIO-PHP-BACKEND/probando/angular-php.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE usuarios(
- 	id int NOT NULL AUTO_INCREMENT,
- 	nombre varchar(50),
- 	dni int,
-
- 	CONSTRAINT usuarios_unique UNIQUE (nombre,dni),
- 	CONSTRAINT usuarios_pk PRIMARY KEY (id)
-)
